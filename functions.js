11556be23cd496981a634ec7089c6e6418589432
Caleb_Zaleski <Caleb.zaleski@icloud.com>
2025-12-05 08:28:52 -0500

    worked on adding details and navbar with the date and time

--- functions.js
+++ functions.js
@@ -12,3 +12,21 @@ function CurTime() {
     const d = new Date()
     return d.toLocaleTimeString();
 }
+
+
+function MQTT (elementId) {
+    const client = new Paho.MQTT.Client("192.168.1.30", 9001, "client_" + Math.random().toString(16).substr(2, 8));
+
+    client.onMessageArrived = function (message) {
+        document.getElementById(elementId).textContent = message.payloadString;
+    };
+
+    client.connect({
+        onSuccess: function () {
+            client.subscribe("Soil_Sensor/topic");
+        }
+    });
+}
+
+
+
